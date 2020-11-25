a0e4d8be3eb7b798a2eb482e773b859305754150
berkth <lingen18@gmail.com>
2020-11-25 17:19:56 +0100

    Update general

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-function toggleMode() {
+function darkMode() {
+	document.getElementById("body").setAttribute("class", "body-dark")
+	document.getElementById("csimg").setAttribute("src", "img/coming-soon-dark.png")
+	document.getElementById("copyimg").setAttribute("src", "img/copyright-small-dark.png")
+	document.getElementById("toggle").setAttribute("class", "lightmode")
 	document.getElementById("toggle").setAttribute("title", "Light Mode");
-	var element1 = document.getElementById("body");
-	element1.classList.toggle("body-dark");
-	var element2 = document.getElementById("csimg");
-	element2.classList.toggle("csimg-dark");
-	var element3 = document.getElementById("copyimg");
-	element3.classList.toggle("copyimg-dark");
-	var element4 = document.getElementById("toggle-img");
-	element4.classList.toggle("lightmode");
+	document.getElementById("toggle").setAttribute("onclick", "lightMode()");
+}
+
+function lightMode() {
+	document.getElementById("body").setAttribute("class", "body")
+	document.getElementById("csimg").setAttribute("src", "img/coming-soon.png")
+	document.getElementById("copyimg").setAttribute("src", "img/copyright-small.png")
+	document.getElementById("toggle").setAttribute("class", "darkmode")
+	document.getElementById("toggle").setAttribute("title", "Dark Mode");
+	document.getElementById("toggle").setAttribute("onclick", "darkMode()");
 }
